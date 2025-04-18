3e0e8f919fb66c89d7973c8cbb9625b23138e47e
Julian <zS22017046@estudiantes.uv.mx>
2025-04-17 22:40:06 -0600

    can now load different views

--- js/app.js
+++ js/app.js
@@ -6,7 +6,22 @@ function loadPartial(id, file) {
     });
 }
 
+function navigateTo() {
+  const hash = window.location.hash.replace("#", "");
+
+  switch (hash) {
+    case "about-us":
+      loadPartial("main-content", "about_us.html");
+      break;
+    default:
+      loadPartial("main-content", "home.html")
+  }
+}
+
+window.addEventListener("hashchange", navigateTo);
+
 document.addEventListener("DOMContentLoaded", () => {
-    loadPartial("header", "header.html")
-    loadPartial("footer", "footer.html")
-})
+  loadPartial("header", "header.html");
+  loadPartial("footer", "footer.html");
+  navigateTo();
+});
