59f996e7334f9eb51e09fd4f81a08d646e2a96b1
Julian <zS22017046@estudiantes.uv.mx>
2025-04-19 00:01:36 -0600

    refactoring fonts and home view, making product list view

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
+const subcategories = {
+  decoracion: ["interior", "exterior"],
+  cocina: ["comales", "fruteros"],
+  organizacion: ["joyeros"],
+};
+
 function loadPartial(id, file) {
   fetch(`partials/${file}`)
     .then((res) => res.text())
@@ -17,10 +23,68 @@ function loadStylesheet(stylesheet) {
   document.head.appendChild(link);
 }
 
+function renderProducts(products) {
+  const div = document.getElementById("product-list");
+  div.innerHTML = products
+    .map((product) => `<h1>${product.name}</h1>`)
+    .join("");
+}
+
+function renderSubcategories(category) {
+  const subList = document.getElementById("subfilter-list");
+  const subNav = document.getElementById("subfilter-nav");
+
+  subList.innerHTML = "";
+
+  if (subcategories[category]) {
+    subcategories[category].forEach((subcategory) => {
+      const li = document.createElement("li");
+      li.innerHTML = `<a href="#" data-category="${category}" data-subcategory="${subcategory}">${subcategory}</a>`;
+      subList.appendChild(li);
+    });
+    subNav.style.display = "block";
+  } else {
+    subNav.style.display = "none";
+  }
+}
+
+function addCategoryEvents(products) {
+  document.querySelectorAll(".filter-nav a").forEach((link) => {
+    link.addEventListener("click", (e) => {
+      e.preventDefault();
+      const category = link.dataset.category;
+      renderSubcategories(category);
+    });
+  });
+}
+
+function initProducts(products) {
+  renderProducts(products);
+  addCategoryEvents(products);
+}
+
+function loadProducts() {
+  fetch("partials/products.html")
+    .then((res) => res.text())
+    .then((html) => {
+      document.getElementById("main-content").innerHTML = html;
+
+      fetch("products.json")
+        .then((res) => res.json())
+        .then((products) => {
+          initProducts(products);
+        });
+    });
+}
+
 function navigateTo() {
   const hash = window.location.hash.replace("#", "");
 
   switch (hash) {
+    case "products":
+      loadStylesheet("products.css");
+      loadProducts();
+      break;
     case "about-us":
       loadStylesheet("about_us.css");
       loadPartial("main-content", "about_us.html");
